f63d13f51e93d7e11a486484cbb84f9ae48aff9f
MariusSavaniauskasKTU <marsav4@ktu.lt>
2023-02-23 16:54:50 +0200

    Žaidėjo judėjimas V.1

---
+++ Unity/MTA/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float moveSpeed = 0.5f;
+    public Vector2 PlayerInput;
+    public Vector2 forceToApply;
+    public float forceDamping = 1.2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            PlayerInput = new Vector2(Input.GetAxisRaw("Horizontal"),
+                Input.GetAxisRaw("Vertical")).normalized;
+    }
+
+    void FixedUpdate()
+    {
+        if (PlayerInput.x > 0)
+        {
+            //rb.SetRotation(-90);
+            //transform.localScale = Vector3.one;
+        }
+        else
+        {
+            if (PlayerInput.x < 0)
+            {
+                //rb.SetRotation(90);
+                //transform.localScale = new Vector3(-1, 1, 1);
+            }
+        }
+
+        Vector2 moveForce = PlayerInput * moveSpeed;
+        moveForce += forceToApply;
+        forceToApply /= forceDamping;
+        if (Mathf.Abs(forceToApply.x) <= 0.01f && Mathf.Abs(forceToApply.y) <= 0.01f)
+        {
+            forceToApply = Vector2.zero;
+        }
+        rb.velocity = moveForce;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Bullet"))
+        {
+            forceToApply += new Vector2(-20, 0);
+            Destroy(collision.gameObject);
+        }
+    }
+}
