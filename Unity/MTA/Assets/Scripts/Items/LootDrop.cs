1f97a7336a714b2c850f6552356c21d3d344a745
MariusM <124732608+FabbyM3@users.noreply.github.com>
2023-03-15 18:59:00 +0200

    Loot v1

---
+++ Unity/MTA/Assets/Scripts/Items/LootDrop.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LootDrop : MonoBehaviour
+{
+    public GameObject droppedItemPrefab;
+    public List<Loot> lootList = new List<Loot>();
+}
