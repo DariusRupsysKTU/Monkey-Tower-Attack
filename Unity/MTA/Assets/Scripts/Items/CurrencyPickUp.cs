4108eb5f370a5b0a85ec6745f4a74c1c3ad52dfc
MariusM <124732608+FabbyM3@users.noreply.github.com>
2023-03-20 20:38:29 +0200

    Currency Fixed

--- Unity/MTA/Assets/Scripts/Items/CurrencyPickUp.cs
+++ Unity/MTA/Assets/Scripts/Items/CurrencyPickUp.cs
@@ -6,13 +6,19 @@ using UnityEngine.EventSystems;
 public class CurrencyPickUp : MonoBehaviour
 {
     public int value = 100;
+    public Inventory inventory;
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void Start()
+    {
+        inventory = GameObject.Find("InventoryCanvas").GetComponent<Inventory>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) 
     {
         if (collision.CompareTag("Player"))
         {
             Destroy(gameObject);
-            Inventory.instance.IncreaseCurrency(value);
+            inventory.IncreaseCurrency(value);
         }
     }
 }
--- Unity/MTA/Assets/Scripts/Items/Inventory.cs
+++ Unity/MTA/Assets/Scripts/Items/Inventory.cs
@@ -6,19 +6,18 @@ using UnityEngine.UI;
 
 public class Inventory : MonoBehaviour
 {
-    public static Inventory instance = new Inventory();
-
+   // public static Inventory instance = new Inventory();
     public static bool inventoryActive = true;
 
     public GameObject inventoryUi;
 
     [Header("Currency")]
-    public int currency = 0;
-    public Text currencyUI;
+    public double currency = 0;
+    public Text currencyUI = null;
 
     public void IncreaseCurrency(int amount)
     {
         currency += amount;
-        currencyUI.text = "Money:" + currency + "$";
+        currencyUI.text = "Money: " + currency + "$";
     }
 }
