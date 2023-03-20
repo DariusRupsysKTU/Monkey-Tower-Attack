5e1b79d7996e995984844abffaddf60427459d49
DariusRupsys <darrup@ktu.lt>
2023-03-20 17:57:00 +0200

    Fixed Enemy1 animation

--- Unity/MTA/Assets/Scripts/Bot/BotHealth.cs
+++ Unity/MTA/Assets/Scripts/Bot/BotHealth.cs
@@ -7,14 +7,18 @@ public class BotHealth : MonoBehaviour
     private Animator anim;
     public int botHealth;
 
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
     public void DamageBot(int amount)
     {
         botHealth -= amount;
-
         if (botHealth <= 0)
         {
-            //Destroy(this.gameObject);
             anim.SetTrigger("bloon1_death");
+            Destroy(this.gameObject, 2f);
         }
     }
 }
