72f40527590f134e665c66efcf1a1e0ab74aae51
MariusM <124732608+FabbyM3@users.noreply.github.com>
2023-03-31 16:34:35 +0300

    Volume Sliders

--- Unity/MTA/Assets/Scripts/Menu/Options.cs
+++ Unity/MTA/Assets/Scripts/Menu/Options.cs
@@ -1,18 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class Options : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] Slider musicSlider;
+    [SerializeField] Slider sfxSlider;
+    public AudioMixer music;
+    public AudioMixer sfx;
+
     void Start()
     {
-        
+        if(!PlayerPrefs.HasKey("musicVolume") || !PlayerPrefs.HasKey("sfxVolume"))
+        {
+            PlayerPrefs.SetFloat("musicVolume", 1);
+            PlayerPrefs.SetFloat("sfxVolume", 1);
+            Load();
+        }
+        else
+        {
+            Load();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetMusicVolume(float volume)
     {
-        
+        music.SetFloat("volume", Mathf.Log10(volume)*20);
+        Save();
+    }
+
+    public void SetSFXVolume(float volume)
+    {
+        sfx.SetFloat("volume", Mathf.Log10(volume) * 20);
+        Save();
+    }
+
+    private void Load()
+    {
+        musicSlider.value = PlayerPrefs.GetFloat("musicVolume");
+        sfxSlider.value = PlayerPrefs.GetFloat("sfxVolume");
+    }
+    
+    private void Save()
+    {
+        PlayerPrefs.SetFloat("musicVolume", musicSlider.value);
+        PlayerPrefs.SetFloat("sfxVolume", sfxSlider.value);
     }
 }
