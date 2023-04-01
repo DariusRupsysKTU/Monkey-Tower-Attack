523ddc60f0c848eaf08e5304cf023bd83347b027
MariusM <124732608+FabbyM3@users.noreply.github.com>
2023-04-01 23:03:36 +0300

    Press buttons with keyboard

--- Unity/MTA/Assets/Scripts/Items/Inventory.cs
+++ Unity/MTA/Assets/Scripts/Items/Inventory.cs
@@ -8,6 +8,81 @@ public class Inventory : MonoBehaviour
 {
     public static Inventory instance { get; private set; }
 
+    [SerializeField] private Button item1;
+    [SerializeField] private Button item2;
+    [SerializeField] private Button item3;
+    [SerializeField] private Button item4;
+
+    public KeyCode key1;
+    public KeyCode key2;
+    public KeyCode key3;
+    public KeyCode key4;
+
+    private void Start()
+    {
+        key1 = KeyCode.Alpha1;
+        key2 = KeyCode.Alpha2;
+        key3 = KeyCode.Alpha3;
+        key4 = KeyCode.Alpha4;
+    }
+
+    void Update()
+    {
+        // Checks if user clicked the button
+        if (Input.GetKeyDown(key1))
+        {
+            FadeToColor(item1.colors.normalColor, item1);
+            item1.onClick.Invoke();
+            Debug.Log("Pressed 1");
+        }
+        else if (Input.GetKeyUp(key1))
+        {
+            FadeToColor(item1.colors.normalColor, item1);
+        }
+
+        if(Input.GetKeyDown(key2))
+        {
+            FadeToColor(item2.colors.pressedColor, item2);
+            item2.onClick.Invoke();
+        }
+        else if(Input.GetKeyUp(key2))
+        {
+            FadeToColor(item2.colors.normalColor, item2);
+        }
+
+        if(Input.GetKeyDown(key3))
+        {
+            FadeToColor(item3.colors.pressedColor, item3);
+            item3.onClick.Invoke();
+        }
+        else if(Input.GetKeyUp(key3))
+        {
+            FadeToColor(item3.colors.normalColor, item3);
+        }
+
+        if(Input.GetKeyDown(key4))
+        {
+            FadeToColor(item4.colors.pressedColor, item4);
+            item4.onClick.Invoke();
+        }
+        else if(Input.GetKeyUp(key4))
+        {
+            FadeToColor(item4.colors.normalColor, item4);
+        }
+
+    }
+
+    void FadeToColor(Color color, Button button)
+    {
+        Graphic graphic = GetComponent<Graphic>();
+        graphic.CrossFadeColor(color, button.colors.fadeDuration, true, true);
+    }
+
+    public void OnItemClicked()
+    {
+        
+    }
+
     private void Awake()
     {
         if (!instance)
