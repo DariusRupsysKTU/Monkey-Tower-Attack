3b7b202c14ca06fd9446b96c4f16f1b22bea570f
MariusSavaniauskasKTU <marsav4@ktu.lt>
2023-05-07 11:25:55 +0300

    Fixed B room visual bug

---
+++ Unity/MTA/Assets/Scripts/Player/PlayerFireball.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerFireball : MonoBehaviour
+{
+    [SerializeField] float bulletSpeed;
+    [SerializeField] int damage;
+    [SerializeField] ParticleSystem rockSplashVFX;
+
+    private UnityEvent onBulletDestroy;
+    private Rigidbody2D bulletRB;
+
+    void Start()
+    {
+        bulletRB = this.GetComponent<Rigidbody2D>();
+        bulletRB.transform.localScale = new Vector3(damage, damage, damage);
+        bulletRB.velocity = transform.up * bulletSpeed;
+        bulletRB.transform.eulerAngles = new Vector3(0f, 0f, bulletRB.transform.eulerAngles.z + 90f);
+        DestroyBullet(1.5f);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        bool enemyIsDead = false;
+
+        EnemyHealth enemyHealthScript = other.GetComponent<EnemyHealth>();
+        if (enemyHealthScript != null)
+        {
+            if (enemyHealthScript.enemyHealth > 0)
+            {
+                enemyHealthScript.DamageEnemy(damage);
+            }
+            else
+            {
+                enemyIsDead = true;
+            }
+        }
+
+        ItemHealth itemHealthScript = other.GetComponent<ItemHealth>();
+        if (itemHealthScript != null)
+        {
+            itemHealthScript.DamageItem(damage);
+        }
+
+        if (other.transform.tag != "Player" && other.transform.tag != "PlayerBullet" && other.transform.tag != "Currency" &&
+        other.transform.tag != "SpawnPoint" && other.transform.tag != "RoomTracker" && other.transform.tag != "RoomChecker" &&
+        !enemyIsDead)
+        {
+            DestroyBullet(0f);
+        }
+    }
+
+    public void EnableDoubleDamage(float time)
+    {
+        damage = damage * 2;
+        Invoke(nameof(DisableDoubleDamage), time);
+    }
+
+    private void DisableDoubleDamage()
+    {
+        damage = damage / 2;
+    }
+
+    public void DisableExtraDamage()
+    {
+        damage = 1;
+    }
+
+    private void PlayRockSplashVFX()
+    {
+        ParticleSystem vfx = Instantiate(rockSplashVFX, this.transform.position, Quaternion.identity);
+        vfx.transform.localScale = new Vector3(damage, damage, damage);
+        vfx.Play();
+        Destroy(vfx.gameObject, rockSplashVFX.main.duration);
+    }
+
+    private void DestroyBullet(float waitTime)
+    {
+        Invoke(nameof(PlayRockSplashVFX), waitTime);
+        Destroy(gameObject, waitTime);
+    }
+}
